e6d1ae757bc0277f3e2da1bdce991ae70464cc97
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-11-03 12:04:53 -0500

    created a querry seclector for the list elements with a class of '.boxx' I used this querySelectorAll to loop an arugment for the list elements. Turning the squares in to cicrles and the circles into squres.

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const box1 = document.getElementById('box1');
-const box2 = document.getElementById('box2');
-const box3 = document.getElementById('box3');
-const box4 = document.getElementById('box4');
-const box5 = document.getElementById('box5');
-const box6 = document.getElementById('box6');
+//const box1 = document.getElementById('box1');
+//const box2 = document.getElementById('box2');
+//const box3 = document.getElementById('box3');
+//const box4 = document.getElementById('box4');
+//const box5 = document.getElementById('box5');
+//const box6 = document.getElementById('box6');
 
+const boxes = document.querySelectorAll(".boxx");
 
-
-function changeBorderRadius() {
-    const borderRadiusSize = box1.style.borderRadius;
-    if (borderRadiusSize < 50) {
-        box1.style.borderRadius = "50px"
+function changeBorderCircle() {
+    for (let i = 0; i < boxes.length; i++) {
+        const borderRadiusSize = boxes[i].style.borderRadius;
+        if (borderRadiusSize < "50px") {
+            boxes[i].style.borderRadius = "50px"
+        } else {
+            boxes[i].style.borderRadius = "0px"
+        }
     }
-} 
+}
 
-box1.addEventListener('click', changeBorderRadius);
+boxes.forEach ((e) => {
+    e.addEventListener('click', changeBorderCircle);
+})
+
+//function changeBorderRadius() {
+    //const borderRadiusSize = box1.style.borderRadius;
+    //if (borderRadiusSize < 50) {
+        //box1.style.borderRadius = "50px"
+    //}
+//} 
+
+//box1.addEventListener('click', changeBorderRadius);
 
